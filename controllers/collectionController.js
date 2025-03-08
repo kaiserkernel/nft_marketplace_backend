ccd73c18e90ed85a4ec3a41b0473b8848ae4fc9c
kaiserkernel <kaiserkernel84@gmail.com>
2025-03-08 08:19:22 +0100

    fix: collection controller

--- controllers/collectionController.js
+++ controllers/collectionController.js
@@ -1,16 +1,25 @@
-const Collection = require('./models/Collection');
+const Collection = require("../models/Collection");
 
-async function saveCollection(data) {
+// Create and save a new collection
+const createCollection = async (req, res) => {
+  try {
+    const { name, description, avatar, banner, metadataURI, contractAddress, creator } = req.body;
+
     const newCollection = new Collection({
-        name: data.name,
-        description: data.description,
-        avatar: data.avatar,
-        banner: data.banner,
-        metadataURI: data.metadataURI,
-        contractAddress: data.contractAddress,
-        creator: data.creator
+      name,
+      description,
+      avatar,
+      banner,
+      metadataURI,
+      contractAddress,
+      creator
     });
 
     await newCollection.save();
-    console.log("Collection saved successfully!");
-}
+    res.status(201).json({ message: "Collection saved successfully!", collection: newCollection });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to save collection", error: error.message });
+  }
+};
+
+module.exports = { createCollection };

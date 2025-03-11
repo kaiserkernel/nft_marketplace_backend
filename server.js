15874e5ba1f6d570ceb8c580465a9fccde04aaca
kaiserkernel <kaiserkernel84@gmail.com>
2025-03-11 16:55:01 +0100

    feat: cors option

--- server.js
+++ server.js
@@ -12,9 +12,20 @@ const app = express();
 // Connect db
 connectDB();
 
+// CORS Configuration
+const corsOptions = {
+    origin: [
+        "http://localhost:3000"
+    ],
+    // origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,  // Allow sending cookies and Authorization headers
+  };
+
 // Middleware
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
 
 // Import routes
 const collectionRoutes = require("./routes/collection");

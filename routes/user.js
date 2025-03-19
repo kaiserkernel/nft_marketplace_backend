448eeb9cbd1b011565fb4a7c5879ef39538d1fc6
kaiserkernel <kaiserkernel84@gmail.com>
2025-03-19 10:55:23 +0100

    feat: upload middleware

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const upload = require("../middlewares/upload"); // Import upload middleware
 const { registerUser } = require("../controllers/userController");
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register",  upload.single("avatar"), registerUser);
 
 module.exports = router;
